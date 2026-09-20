f9023df5b802767347bd56ac0b765615f4941b1e
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-09-20 13:58:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Check Equal Arrays/check-equal-arrays.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool checkEqual(vector<int>& a, vector<int>& b) {
+        // code here
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+        bool h = true;
+        for(int i = 0; i < a.size(); i++){
+            if(a[i] != b[i]){
+                h = false ;
+            }
+        }
+        return h;
+    }
+};
