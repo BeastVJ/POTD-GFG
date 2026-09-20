5467ef3508198701e768bca955aac7b66d64aa9f
Vijay Singh Bisht <129999814+BeastVJ@users.noreply.github.com>
2026-09-20 13:32:18 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/k Largest Elements/k-largest-elements.cpp
@@ -0,0 +1,13 @@
+class Solution {
+  public:
+    vector<int> kLargest(vector<int>& arr, int k) {
+        // code here
+        sort(arr.rbegin(), arr.rend());
+        vector<int> ans;
+        for(int i = 0 ; i < k; i++){
+            ans.push_back(arr[i]);
+        }
+
+        return ans;
+    }
+};
